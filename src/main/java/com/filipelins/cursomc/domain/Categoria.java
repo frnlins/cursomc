428ea280c4e75f02664490b0b30ed2c4db617ceb
Filipe Lins <frnlins@gmail.com>
2019-09-15 08:23:15 -0300

    configuração do BD H2 para teste, geração automática das tabelas

--- src/main/java/com/filipelins/cursomc/domain/Categoria.java
+++ src/main/java/com/filipelins/cursomc/domain/Categoria.java
@@ -2,15 +2,22 @@ package com.filipelins.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-public class Categoria implements Serializable {
+public @Entity class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private final Integer id;
 	private final String nome;
 }
